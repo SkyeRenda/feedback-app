54f29109ffbd7305822560f414a28a5ebef4d183
SkyeRenda <skyerenda@hotmail.com>
2022-09-03 14:35:48 +0200

    finshed understanding JSX

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header text={true}></Header>
+      <div>
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
