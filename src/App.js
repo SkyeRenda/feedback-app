46d019e1ef98c2a1ce3b87f7e3f7fa395c26eee9
SkyeRenda <skyerenda@hotmail.com>
2022-09-03 15:59:43 +0200

    addec formatting to cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedBack] = useState(FeedbackData);
+
   return (
     <>
-      <Header text={true}></Header>
-      <div>
-        <h1>My App</h1>
+      <Header></Header>
+      <div className="container">
+        <FeedbackList feedback={feedback}></FeedbackList>
       </div>
     </>
   );
